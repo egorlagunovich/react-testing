1fa8af7a7f54bdf01976987211eca0fe4c20943b
egorlagunovich <egorlagunovich@gmail.com>
2023-05-20 12:10:06 +0200

    adding describe

--- src/Greet.test.tsx
+++ src/Greet.test.tsx
@@ -1,14 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import Greet from "./Greet";
 
-test("greet renders correctly", () => {
-  render(<Greet />);
-  const greeting = screen.getByText("Hello John");
-  expect(greeting).toBeInTheDocument();
-});
+describe("greet", () => {
+  test("greet renders correctly", () => {
+    render(<Greet />);
+    const greeting = screen.getByText("Hello John");
+    expect(greeting).toBeInTheDocument();
+  });
 
-test("greet name renders correctly", () => {
-  render(<Greet name="Jurek" />);
-  const greeting = screen.getByText("Hello Jurek");
-  expect(greeting).toBeInTheDocument();
+  test("greet name renders correctly", () => {
+    render(<Greet name="Jurek" />);
+    const greeting = screen.getByText("Hello Jurek");
+    expect(greeting).toBeInTheDocument();
+  });
 });
